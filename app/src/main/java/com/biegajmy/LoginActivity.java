6277e8dbcc329979b41626574d41e90c47845b71
Mateusz Korszun <mkorszun@gmail.com>
2015-08-27 11:27:46 +0200

    Add splunk mint monitoring

--- app/src/main/java/com/biegajmy/LoginActivity.java
+++ app/src/main/java/com/biegajmy/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import com.biegajmy.backend.UserBackendService_;
 import com.biegajmy.location.LocationService_;
+import com.splunk.mint.Mint;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -12,10 +13,9 @@ public class LoginActivity extends ActionBarActivity {
         setContentView(R.layout.activity_login);
         LocationService_.intent(getApplication()).start();
         UserBackendService_.intent(getApplication()).start();
+        Mint.initAndStartSession(this, BuildConfig.MINT_TOKEN);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, new LoginFragment_())
-                .commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new LoginFragment_()).commit();
         }
     }
 }
